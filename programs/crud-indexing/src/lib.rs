d6ccddca79e9ac23af6ef23f918649d514237e4e
ngundotra <noah@gundotra.org>
2023-06-01 14:50:18 -0400

    sync name changes

--- programs/crud-indexing/src/lib.rs
+++ programs/crud-indexing/src/lib.rs
@@ -1,15 +1,192 @@
 use anchor_lang::prelude::*;
+use anchor_lang::Discriminator;
+use serde::{self, Deserialize, Deserializer, Serialize, Serializer};
+use serde_json;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+mod bs58_pubkey;
+use bs58_pubkey::serde_pubkey;
+
+declare_id!("3YtbN69K6qkLsc3nWCPrzSkyNmwzF5uRppmfm9FyYz4k");
+
+pub const SYMBOL_MAX_LEN: usize = 8;
+pub const URI_MAX_LEN: usize = 64;
+pub const NAME_MAX_LEN: usize = 64;
 
 #[program]
 pub mod crud_indexing {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn init_collection(ctx: Context<InitCollection>, num_items: u32) -> Result<()> {
+        ctx.accounts.collection.authority = *ctx.accounts.owner.key;
+        ctx.accounts.collection.num_items = num_items;
+
+        // Issue a collection
+        emit_cpi!({
+            CrudCreate {
+                authority: ctx.accounts.owner.key(),
+                asset_id: ctx.accounts.collection.key(),
+                pubkeys: vec![ctx.accounts.collection.key().clone()],
+                data: vec![],
+            }
+        });
+        Ok(())
+    }
+
+    pub fn mint(
+        ctx: Context<MintMe>,
+        collection_num: u32,
+        name: String,
+        symbol: String,
+        uri: String,
+    ) -> Result<()> {
+        ctx.accounts.metadata.name = name.clone();
+        ctx.accounts.metadata.symbol = symbol.clone();
+        ctx.accounts.metadata.uri = uri.clone();
+        ctx.accounts.metadata.owner = *ctx.accounts.owner.key;
+
+        // Issue a metadata
+        emit_cpi!({
+            CrudCreate {
+                authority: ctx.accounts.owner.key(),
+                asset_id: ctx.accounts.metadata.key(),
+                pubkeys: vec![
+                    ctx.accounts.collection.key().clone(),
+                    ctx.accounts.metadata.key().clone(),
+                ],
+                data: vec![],
+            }
+        });
+
+        Ok(())
+    }
+
+    pub fn transfer(ctx: Context<TransferMe>, collection_num: u32) -> Result<()> {
+        ctx.accounts.metadata.owner = *ctx.accounts.owner.key;
+
+        emit_cpi!({
+            CrudTransfer {
+                asset_id: ctx.accounts.metadata.key().clone(),
+                new_authority: ctx.accounts.dest.key().clone(),
+            }
+        });
+
+        Ok(())
+    }
+
+    pub fn get_asset_data(ctx: Context<GetAssetDataAccounts>, data: Vec<u8>) -> Result<()> {
+        let data = ctx.remaining_accounts[1].try_borrow_mut_data()?;
+
+        let account_disc = &data[0..8];
+        let json_data = if *account_disc == Metadata::DISCRIMINATOR {
+            let metadata_info = Metadata::try_from_slice(&data[8..data.len()])?;
+
+            let data = serde_json::to_string(&metadata_info).unwrap();
+            msg!("Found metadata: {}", &data);
+            data
+        } else if *account_disc == Collection::DISCRIMINATOR {
+            let collection_info = Collection::try_from_slice(&data[8..data.len()])?;
+            let data = serde_json::to_string(&collection_info).unwrap();
+            msg!("Found collection: {}", &data);
+            data
+        } else {
+            "".to_string()
+        };
+        anchor_lang::solana_program::program::set_return_data(&json_data.as_bytes());
         Ok(())
     }
 }
 
+// pub fn account_discriminator(account_name: &str) -> [u8; 8] {
+//     let discriminator_preimage = { format!("account:{account_name}") };
+
+//     let mut discriminator = [0u8; 8];
+//     discriminator.copy_from_slice(
+//         &anchor_lang::solana_program::hash::hashv(&[discriminator_preimage.as_bytes()]).to_bytes()
+//             [..8],
+//     );
+//     return discriminator;
+// }
+
+#[derive(Debug, Serialize)]
+#[account]
+pub struct Metadata {
+    #[serde(with = "serde_pubkey")]
+    owner: Pubkey,
+    name: String,
+    symbol: String,
+    uri: String,
+}
+
+#[derive(Debug, Serialize)]
+#[account]
+pub struct Collection {
+    #[serde(with = "serde_pubkey")]
+    pub authority: Pubkey,
+    pub num_items: u32,
+}
+
+#[event_cpi]
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitCollection<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(init, payer=owner, space = 8 + 32 + 4)]
+    pub collection: Account<'info, Collection>,
+    pub system_program: Program<'info, System>,
+}
+
+#[event_cpi]
+#[derive(Accounts)]
+#[instruction(collection_num: u32, name: String, symbol: String, uri: String)]
+pub struct MintMe<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub collection: Account<'info, Collection>,
+    #[account(init, payer=owner, space = 8 + 32 + 4 + name.len() + 4 + symbol.len() + 4 + uri.len(), seeds = [collection.key().as_ref(), b"metadata".as_ref(), &collection_num.to_le_bytes()], bump)]
+    pub metadata: Account<'info, Metadata>,
+    pub system_program: Program<'info, System>,
+}
+
+#[event_cpi]
+#[derive(Accounts)]
+#[instruction(collection_num: u32)]
+pub struct TransferMe<'info> {
+    pub owner: Signer<'info>,
+    /// CHECK: recipient
+    pub dest: AccountInfo<'info>,
+    pub collection: Account<'info, Collection>,
+    #[account(mut, seeds = [collection.key().as_ref(), b"metadata".as_ref(), &collection_num.to_le_bytes()], bump)]
+    pub metadata: Account<'info, Metadata>,
+}
+
+#[derive(Accounts)]
+pub struct GetAssetDataAccounts<'info> {
+    /// CHECK:
+    pub asset_id: AccountInfo<'info>,
+}
+
+#[event]
+pub struct CrudCreate {
+    asset_id: Pubkey,
+    authority: Pubkey,
+    pubkeys: Vec<Pubkey>,
+    data: Vec<u8>,
+}
+
+#[event]
+pub struct CrudTransfer {
+    asset_id: Pubkey,
+    new_authority: Pubkey,
+}
+
+#[event]
+pub struct CrudUpdate {
+    asset_id: Pubkey,
+    pubkeys: Vec<Pubkey>,
+    data: Vec<u8>,
+}
+
+#[event]
+pub struct CrudDelete {
+    asset_id: Pubkey,
+}
--- tests/crud-indexing.ts
+++ tests/crud-indexing.ts
@@ -1,6 +1,155 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CrudIndexing } from "../target/types/crud_indexing";
+import { TransactionInstruction } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
+
+type UniversalIx = {
+  programId: anchor.web3.PublicKey;
+  keys: anchor.web3.PublicKey[];
+  data: Buffer;
+};
+
+function orderInstructions(tx: anchor.web3.TransactionResponse): UniversalIx[] {
+  let accounts: anchor.web3.PublicKey[] =
+    tx.transaction.message.staticAccountKeys;
+  accounts = accounts.concat(tx.meta.loadedAddresses.writable ?? []);
+  accounts = accounts.concat(tx.meta.loadedAddresses.readonly ?? []);
+
+  let ordered: UniversalIx[] = [];
+
+  let outerIdx = 0;
+  let innerIdx = 0;
+  for (const outerIxFlat of tx.transaction.message.instructions) {
+    let outerIx: UniversalIx = {
+      programId: accounts[outerIxFlat.programIdIndex],
+      keys: outerIxFlat.accounts.map((idx) => accounts[idx]),
+      data: anchor.utils.bytes.bs58.decode(outerIxFlat.data),
+    };
+    ordered.push(outerIx);
+
+    const innerIxBucket = tx.meta?.innerInstructions[innerIdx];
+    if (innerIxBucket && innerIxBucket.index === outerIdx) {
+      for (const innerIxFlat of innerIxBucket.instructions) {
+        let innerIx: UniversalIx = {
+          programId: accounts[innerIxFlat.programIdIndex],
+          keys: innerIxFlat.accounts.map((idx) => accounts[idx]),
+          data: anchor.utils.bytes.bs58.decode(innerIxFlat.data),
+        };
+        ordered.push(innerIx);
+      }
+      innerIdx += 1;
+    }
+    outerIdx += 1;
+  }
+  return ordered;
+}
+
+const CPI_EVENT_IX: Buffer = Buffer.from([
+  0xe4, 0x45, 0xa5, 0x2e, 0x51, 0xcb, 0x9a, 0x1d,
+]);
+
+// Parses CPI events from a transaction for the given anchor program
+function parseCpiEvents(
+  tx: anchor.web3.TransactionResponse,
+  program: anchor.Program
+): anchor.Event[] {
+  let orderedIxs = orderInstructions(tx);
+  // console.log(orderedIxs);
+
+  let events: anchor.Event[] = [];
+  for (let i = 1; i < orderedIxs.length; i += 1) {
+    const ix = orderedIxs[i];
+
+    if (ix.data.slice(0, 8).equals(CPI_EVENT_IX)) {
+      const eventAuthority = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("__event_authority")],
+        program.programId
+      )[0];
+
+      // CHECK that the event authority is the CPI authority
+      if (
+        ix.keys.length != 1 ||
+        ix.keys[0].toBase58() !== eventAuthority.toBase58()
+      ) {
+        continue;
+      }
+
+      const eventData = anchor.utils.bytes.base64.encode(ix.data.slice(8));
+      const event = program.coder.events.decode(eventData);
+      events.push(event);
+    }
+  }
+
+  return events;
+}
+
+async function interpretEvents(
+  events: anchor.Event[],
+  program: anchor.Program<CrudIndexing>
+) {
+  for (const event of events) {
+    if (event.name == "CrudCreate" || event.name == "CrudUpdate") {
+      let assetId: PublicKey = event.data.assetId as any;
+      let pubkeys: PublicKey[] = event.data.pubkeys as any;
+      let ix_data: Buffer = event.data.data as any;
+
+      let ix = await program.methods
+        .getAssetData(ix_data)
+        .accounts({ assetId })
+        .remainingAccounts(
+          pubkeys.map((key) => {
+            return {
+              isSigner: false,
+              isWritable: false,
+              pubkey: key,
+            };
+          })
+        )
+        .instruction();
+      const mv0 = anchor.web3.MessageV0.compile({
+        instructions: [ix],
+        payerKey: program.provider.publicKey!,
+        recentBlockhash: (
+          await program.provider.connection.getLatestBlockhash()
+        ).blockhash,
+      });
+      const vtx = new anchor.web3.VersionedTransaction(mv0);
+
+      let simulationResult = await program.provider
+        .simulate(vtx, [], "confirmed")
+        .catch((err) => console.error(err));
+      if (!simulationResult) {
+        continue;
+      }
+
+      let returnData = simulationResult.returnData;
+      if (returnData) {
+        let returnDataLog =
+          simulationResult.logs[simulationResult.logs.length - 2];
+        let subjects = returnDataLog.split(" ");
+
+        let programId = subjects[2];
+        let retData = subjects[3];
+
+        if (programId !== program.programId.toBase58()) {
+          throw new Error("Program ID mismatch in return data");
+        }
+
+        let data = anchor.utils.bytes.base64.decode(retData);
+        console.log(
+          `${event.name.replace("Crud", "")}-d asset for ${
+            event.data.authority
+          }: ${data.toString("utf-8")} (${event.data.assetId})})`
+        );
+      }
+    } else if (event.name == "CrudDelete") {
+      console.log(
+        `Deleted asset for ${event.data.authority}: ${event.data.assetId}`
+      );
+    }
+  }
+}
 
 describe("crud-indexing", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +157,60 @@ describe("crud-indexing", () => {
 
   const program = anchor.workspace.CrudIndexing as Program<CrudIndexing>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let collectionKp = anchor.web3.Keypair.generate();
+  let collection = collectionKp.publicKey;
+
+  it("Can create a collection", async () => {
+    const tx = await program.methods
+      .initCollection(10000)
+      .accounts({
+        owner: program.provider.publicKey,
+        collection,
+      })
+      .signers([collectionKp])
+      .rpc({ commitment: "confirmed" });
+
+    const txResult = await program.provider.connection.getTransaction(tx, {
+      commitment: "confirmed",
+    });
+
+    let events = parseCpiEvents(txResult, program);
+    await interpretEvents(events, program);
+  });
+  it("Can mint an NFT", async () => {
+    const tx = await program.methods
+      .mint(0, "name", "symbol", "uri")
+      .accounts({
+        owner: program.provider.publicKey,
+        collection,
+      })
+      .rpc({ commitment: "confirmed" });
+
+    const txResult = await program.provider.connection.getTransaction(tx, {
+      commitment: "confirmed",
+    });
+
+    let events = parseCpiEvents(txResult, program);
+    await interpretEvents(events, program);
+  });
+  it("Can transfer an NFT", async () => {
+    const tx = await program.methods
+      .transfer(0)
+      .accounts({
+        owner: program.provider.publicKey,
+        dest: anchor.web3.Keypair.generate().publicKey,
+        collection,
+      })
+      .rpc({ commitment: "confirmed" });
+
+    const txResult = await program.provider.connection.getTransaction(tx, {
+      commitment: "confirmed",
+    });
+
+    // let events = parseCpiEvents(txResult, program);
+    // console.log(events);
+
+    let events = parseCpiEvents(txResult, program);
+    await interpretEvents(events, program);
   });
 });
